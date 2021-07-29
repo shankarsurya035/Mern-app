42f0aaf419eb286231b3ae8a07e0b8405adee2f1
shankarsurya035 <shankarshakti035@gmail.com>
2021-07-29 20:08:55 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+      <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>
+        About
+      </h1>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Welcome to my Blog! 
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+      Hi there ! I am Surya Shankar . Skilled in Java (Programming Language),
+       C Programing, Advanced Java(Full Stack java),and Web Development (HTML,CSS,JAVASCRIPT) 
+       With Wordpress . Strong business development professional with a Bachelor's
+       degree focused in Electronics and Communication from Silicon Institute of Technology (SIT),
+        Bhubaneswar.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque possimus
+        perspiciatis, in natus illo, dolore porro sequi vitae neque expedita
+        maxime ratione aspernatur hic at exercitationem doloremque cumque quo!
+        Hic provident minima dignissimos maxime eum molestias non illo cum! Eius
+        tempora nisi quisquam incidunt molestiae voluptatum delectus pariatur
+        rerum minima, inventore consequatur. Aut velit dolore in, iusto cumque
+        culpa adipisci quisquam unde suscipit vel ipsa odit praesentium
+        voluptate possimus reprehenderit ex similique repellendus nemo magni
+        soluta consequuntur aliquid? Qui ea hic soluta eaque ad ipsam! Aliquam
+        ea maiores enim, perspiciatis hic deleniti minima pariatur quaerat
+        placeat tempora repellat sit nisi!
+      </p>
+    </div>
+  );
+};
+
+export default About;
