7c18833f11ad8878efe9ebc6551c9db3ceac4fc8
Surya shankar <82276019+shankarsurya035@users.noreply.github.com>
2021-08-12 12:52:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// Pages
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ArticlesList from "./pages/ArticlesList";
